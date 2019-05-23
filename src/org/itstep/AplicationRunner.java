95920c90ae2121952aed9f9d0ef5a41670b2cdee
artempedcenko <Spartanec1@gmail.com>
2019-05-23 19:43:03 +0300

    Initial commit

---
+++ src/org/itstep/AplicationRunner.java
@@ -0,0 +1,18 @@
+package org.itstep;
+
+public class AplicationRunner {
+    String text;
+
+    public static void main(String[] args) {
+        someMethod("Hi! My name is Artem!");
+
+
+    }
+
+    static void someMethod(String msg) {
+        System.out.println(msg);
+    }
+
+}
+
+
